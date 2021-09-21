d54a84d81feb4f98b80fd42865825e43902d848b
yusufjameel84 <yusufjameel84@gmail.com>
2021-09-21 18:57:19 +0530

    Header Footer

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Container , Row , Col} from 'react-bootstrap'
+
+const Footer = () => {
+  return (
+    <footer className="text-centre py-3" >
+      <Container>
+        <Row>
+          <Col style={{textAlign: "center"}}>
+            Copyright &copy; Lucin
+          </Col>
+        </Row>
+          
+      </Container>
+    </footer>
+  )
+}
+
+export default Footer
