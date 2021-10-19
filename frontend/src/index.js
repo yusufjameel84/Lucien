9f3efaf424312392f5cdff48404ff5a8209e2630
yusufjameel84 <yusufjameel84@gmail.com>
2021-10-19 19:15:36 +0530

    redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './bootstrap.min.css'
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Provider } from "react-redux"
+import store from "./store"
+import "./bootstrap.min.css"
+import "./index.css"
+import App from "./App"
+import reportWebVitals from "./reportWebVitals"
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  </Provider>,
+  document.getElementById("root")
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
