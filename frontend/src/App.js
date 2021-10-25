eb169ddbf3e1075306fe37f90eb323d9fe90d59a
yusufjameel84 <yusufjameel84@gmail.com>
2021-10-25 18:32:21 +0530

    update profile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import ProductScreen from "./screens/ProductScreen"
 import CartScreen from "./screens/CartScreen"
 import LoginScreen from "./screens/LoginScreen"
 import RegisterScreen from "./screens/RegisterScreen"
+import ProfileScreen from "./screens/ProfileScreen"
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
       <main className="py-3">
         <Container>
           <Route path="/login" component={LoginScreen} exact />
+          <Route path="/profile" component={ProfileScreen} exact />
           <Route path="/register" component={RegisterScreen} exact />
           <Route path="/product/:id" component={ProductScreen} exact />
           <Route path="/cart/:id?" component={CartScreen} exact />
