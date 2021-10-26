c804ac4ab192911fbb65f022993e1f95607ee5f9
yusufjameel84 <yusufjameel84@gmail.com>
2021-10-26 23:14:13 +0530

    shipping added

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -31,10 +31,17 @@ const cartItemsFromStorage = localStorage.getItem("cartItems")
   ? JSON.parse(localStorage.getItem("userInfo"))
   : null
 
-const initialState = {
-  cart: { cartItems: cartItemsFromStorage },
-  userLogin: { userInfo : userInfoFromStorage }
-}
+  const shippingInfoFromStorage = localStorage.getItem("shippingAddress")
+    ? JSON.parse(localStorage.getItem("shippingAddress"))
+    : {}
+
+  const initialState = {
+    cart: {
+      cartItems: cartItemsFromStorage,
+      shippingAddress: shippingInfoFromStorage,
+    },
+    userLogin: { userInfo: userInfoFromStorage },
+  }
 
 const middleware = [thunk]
 
