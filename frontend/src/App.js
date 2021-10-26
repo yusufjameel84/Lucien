90b515bfaac3fa9ee063ea5196a34df12aee483a
yusufjameel84 <yusufjameel84@gmail.com>
2021-10-26 23:35:47 +0530

    payment screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import LoginScreen from "./screens/LoginScreen"
 import RegisterScreen from "./screens/RegisterScreen"
 import ProfileScreen from "./screens/ProfileScreen"
 import ShippingScreen from "./screens/ShippingScreen"
+import PaymentScreen from "./screens/PaymentScreen"
 
 const App = () => {
   return (
@@ -18,8 +19,9 @@ const App = () => {
       <main className="py-3">
         <Container>
           <Route path="/login" component={LoginScreen} exact />
-          <Route path="/shipping" component={ShippingScreen} exact />
+          <Route path="/payment" component={PaymentScreen} exact />
           <Route path="/profile" component={ProfileScreen} exact />
+          <Route path="/shipping" component={ShippingScreen} exact />
           <Route path="/register" component={RegisterScreen} exact />
           <Route path="/product/:id" component={ProductScreen} exact />
           <Route path="/cart/:id?" component={CartScreen} exact />
