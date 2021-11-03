b4e2d6a4b65b791221541ff379e1c08a88c8fcc6
yusufjameel84 <yusufjameel84@gmail.com>
2021-11-03 18:35:07 +0530

    big fix

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -11,7 +11,7 @@ const orderSchema = mongoose.Schema(
       {
         name: { type: String, required: true },
         qty: { type: Number, required: true },
-        img: { type: String, required: true },
+        img: { type: String },
         price: { type: Number, required: true },
         product: {
           type: mongoose.Schema.Types.ObjectId,
